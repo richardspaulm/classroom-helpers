fb6f291aef996afec510e95e14e8682266568e47
richardspaulm <richardspaulm@gmail.com>
2018-07-28 02:15:11 +0700

    Anagram jQuery Remake

---
+++ js/anagramQuery.js
@@ -0,0 +1,117 @@
+var totalWords = 8;
+window.onload = function(){
+/*Hide/Show Instruction popup*/
+//Show Instructions
+$("#scrollButtonTools").click(function(){
+	$("#toolInstructions").show();
+});
+//Hide Instructions
+$("#instructionButton").click(function(){
+	$("#toolInstructions").hide();
+});
+
+/*Random Puzzle or Template Select*/
+
+//Set Default click to Random
+$("#printButton").click(PrintBoards);
+
+//Event Listener for radio buttons
+var random = true;
+$("#randomRadio, #blankRadio").click(function(sender){
+	if($(this).val() == "random")
+	{
+		$("#divToHide").show();
+		random = true;
+	} 
+	else if($(this).val() == "blank")
+	{
+		$("#divToHide").hide();
+		random = false;
+	}
+});
+
+/*Textarea Word Counter*/
+$("#bingoInput").keypress(function(){
+	var editString = $("#bingoInput").val().replace(/\n/g, ",").split(",");
+	$("#wordCount").html(editString.length.toString());
+});
+/*Size Select*/
+var sizeSelect = $("#wordSearchSizeSelect")
+sizeSelect.change(function(){
+	totalWords = $("#wordSearchSizeSelect option:selected").val();
+	console.log(totalWords);
+});
+function PrintBoards(){
+	$("printable").html("");
+	var numPages;
+	if(!random)
+		numPages = 1;
+	else
+		numPages = $("#bingoStudents").val();
+	var wordArray = $("#bingoInput").val().replace(/\n/g, ",").split(",");
+	if(totalWords > wordArray.length){
+		alert("Not enough words! Either add more words above, or choose a smaller word count!");
+		return false;
+	}
+/*MAIN PAGE LOOP*/
+	for(k=0; k < numPages; k++){
+		tableData = "";
+			var wordArray = $("#bingoInput").val().replace(/\n/g, ",").split(",");
+		var printDiv = $("#printable").append("<div class='randomPrintable' id='randomPrintable" + k.toString() + "'>");
+	//Add Default Header if Title input is blank, or user-entered header
+		if($("#bingoTitle").val() == "")
+		$("#randomPrintable" + k.toString()).append("<h1 id='bingoHead'>Anagrams</h1>");
+		else if($("#bingoTitle").val() != "")
+		$("#randomPrintable" + k.toString()).append("<h1 id='bingoHead'>" + $("#bingoTitle").val() + "</h1>");
+	
+
+	var tableHtml = "<div id='anagramTable" + totalWords.toString() + "'>";
+	for(i=0; i < (totalWords/2); i++){
+		tableHtml += "<div>"
+		for(ii=0; ii < 2; ii++){
+			tableHtml += "<div id='anagramBlock'><h2 class='anagramText'></h2><div id='anagramTopDiv'></div><div id='anagramBotDiv'></div></div>";
+		}
+		tableHtml += "</div>"
+	}
+
+	$("#randomPrintable" + k.toString()).append(tableHtml);
+	var tableData = $("#randomPrintable" + k.toString() + " .anagramText");
+	for(j=0; j < totalWords; j++)
+	{
+		var jumbledWord = jumble_word(wordArray[j]);
+		if(jumbledWord = wordArray[j])
+			jumbledWord = jumble_word(jumbledWord);
+		tableData[j].textContent += jumbledWord;
+	}
+	var drawHtml = "<div id='drawDiv" + totalWords.toString() + "'>" +
+	"<p id='drawP'>Draw your favorite words here!</p></div>";
+	 $("#randomPrintable" + k.toString()).append(drawHtml)
+	}//End Page Loop
+//Print
+	var printContents = $("#printable").html()
+     var printWindow = window.open()
+     printWindow.document.open();
+     printWindow.document.write('<html><head><title>Bingo Printout</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"><link rel="stylesheet" type="text/css" href="css/styleSheet.css"><style type="text/css" media="print">@page{size: auto;margin: 0;}</style></head><body onfocus="setTimeout(function() {window.print(); window.close(); }, 100);">')
+ 	 printWindow.document.write($("#printable").html());
+ 	 printWindow.document.write('<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script></body></html>');
+ 	 //$("#printable").html("");
+}
+function jumble_word(str)
+{
+	var startingWord = str;
+	var newWord = "";
+	while(str.length > 0)
+	{
+		var charSpot = Math.floor(Math.random() * str.length);
+		newWord += str.charAt(charSpot);
+		str = remove_character(str, charSpot);
+	}
+	return newWord;
+}	
+function remove_character(str, char_pos) 
+ {
+  part1 = str.substring(0, char_pos);
+  part2 = str.substring(char_pos + 1, str.length);
+  return (part1 + part2);
+ }
+}
