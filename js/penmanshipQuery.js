bc4274e0ca8119cbaacf0e2da773e87147583d9f
richardspaulm <richardspaulm@gmail.com>
2018-07-28 14:15:07 +0700

    jquery remake and print update

---
+++ js/penmanshipQuery.js
@@ -0,0 +1,96 @@
+var totalWords = 14;
+window.onload = function(){
+DrawForm();
+/*Hide/Show Instruction popup*/
+//Show Instructions
+$("#scrollButtonTools").click(function(){
+	$("#toolInstructions").show();
+});
+//Hide Instructions
+$("#instructionButton").click(function(){
+	$("#toolInstructions").hide();
+});
+var sizeSelect = $("#wordSearchSizeSelect")
+sizeSelect.change(function(){
+	totalWords = $("#wordSearchSizeSelect option:selected").val();
+	DrawForm();
+});
+
+//Set Print Button
+$("#printButton").click(PrintPenmanship);
+function DrawForm(){
+	$("#entryDiv").html("");
+	var tableHtml = "<table id='penmanshipEntry'>"
+	if(totalWords == 14){
+		for(i=0; i < totalWords; i++){
+			tableHtml += "<tr><td><input id='formInput'></td></tr>";
+		}
+	}
+	else if(totalWords == 28)
+	{
+		for(j=0; j < (totalWords/2); j++){
+			tableHtml += "<tr><td><input id='formInput20'></td><td><input id='formInput20'></td></tr>";
+		}
+	}
+	tableHtml += "</table>";
+	$("#entryDiv").append(tableHtml);
+
+}
+function PrintPenmanship(){
+	var printDiv = $("#printable").append("<div class='randomPrintable' id='randomPrintable'>");
+//Add Default Header if Title input is blank, or user-entered header
+	if($("#bingoTitle").val() == "")
+	$("#randomPrintable").append("<h1 id='bingoHead'>Penmanship!</h1>");
+	else if($("#bingoTitle").val() != "")
+	$("#randomPrintable").append("<h1 id='bingoHead'>" + $("#bingoTitle").val() + "</h1>");
+	
+	$("#randomPrintable").append("<div id='linesArea'></div><div id='wordsDiv'></div>");
+	var wordsHtml = "";
+	//14 Words Loop
+	if(totalWords == 14){
+		for(k=0; k < totalWords; k++){
+			var linesHtml = "<div id='penmanshipLines'><div id='penmanshipDots'></div></div>";
+			$("#linesArea").append(linesHtml);
+		} 
+		var count = 0;
+		$("input").each(function(){
+			if(count != 0)
+			wordsHtml += "<p id='entryWord' style='top: " + (count * 82).toString() +  "px;'>" + $(this).val() + "</p>";
+			count ++;
+		});
+		$("#wordsDiv").append(wordsHtml);
+	}
+	else if(totalWords == 28){
+		for(l=0; l < totalWords; l++){
+			var linesHtml = "<div id='penmanshipLines20'><div id='penmanshipDots'></div></div>";
+			$("#linesArea").append(linesHtml);
+		}
+		var count = 0; //To iterate through inputs. Exclude 1st input (its the form title)
+		var rowCounter = 1;//Iterate through lines rows
+		console.log($("td input").length);
+		$("td input").each(function(){
+			if(count % 2 == 0){
+				wordsHtml += "<p id='entryWord20' style='top: "
+				 + (rowCounter * 80).toString()
+				  + "px;'>" + $(this).val() + "</p>";
+				}
+			else if(count % 2 != 0){
+				wordsHtml += "<p id='entryWord20' style='left: 500px; top: "
+								+ (rowCounter * 80).toString()
+								 + "px;'>" + $(this).val() + "</p>";
+			rowCounter++;
+			}
+			count++;
+		});
+		$("#wordsDiv").append(wordsHtml);
+	}
+//Print
+	var printContents = $("#printable").html()
+     var printWindow = window.open()
+     printWindow.document.open();
+     printWindow.document.write('<html><head><title>Bingo Printout</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"><link rel="stylesheet" type="text/css" href="css/styleSheet.css"><style type="text/css" media="print">@page{size: auto;margin: 0;}</style></head><body id = "printBody"><script>setTimeout(function(){window.print(); window.close()}, 200)</script></body>')
+ 	 printWindow.document.write(printContents);
+ 	 printContents = "";
+}
+
+}
--- js/wordsearchQuery.js
+++ js/wordsearchQuery.js
@@ -240,9 +240,9 @@ function PrintEm(){
 	var printContents = $("#printable").html()
      var printWindow = window.open()
      printWindow.document.open();
-     printWindow.document.write('<html><head><title>Bingo Printout</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"><link rel="stylesheet" type="text/css" href="css/styleSheet.css"><style type="text/css" media="print">@page{size: auto;margin: 0;}</style></head><body onfocus="setTimeout(function() {window.print(); window.close(); }, 100);">')
- 	 printWindow.document.write($("#printable").html());
- 	 printWindow.document.write('<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script></body></html>');
- 	 $("#printable").html("");
+     printWindow.document.write('<html><head><title>Bingo Printout</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"><link rel="stylesheet" type="text/css" href="css/styleSheet.css"><style type="text/css" media="print">@page{size: auto;margin: 0;}</style></head><body id = "printBody"><script>setTimeout(function(){window.print(); window.close()}, 1)</script></body>')
+ 	 printWindow.document.write(printContents);
+ 	 printContents = "";
+
 }//End of Print function
 }//End of Doc Ready
